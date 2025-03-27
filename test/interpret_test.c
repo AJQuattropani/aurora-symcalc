0ef3f90c37aeeb2d07a3dd0ca1bb91b7f2043365
AJ Quattropani <AJQuattropani@github.com>
2025-03-26 23:08:28 -0600

    Added another string for testing.

--- test/interpret_test.c
+++ test/interpret_test.c
@@ -6,16 +6,20 @@
 
 int main() {
   char* string = "x^2-(1+2.5(x/2.2)^3)-3.3/x\n";
-  //build_function(string, strlen(string));
-  
-  printf("%s", string);
+
+  printf("%s\n", string);
 
   struct token_list tokens = parse_string_tokens(string, strlen(string));
   
   print(&tokens);
+  
+  char* string2 = "(x+1.1^1.1)(e^x-x*x)  - 2.2 \n";
+
+  struct token_list tokens2 = parse_string_tokens(string2, strlen(string2));
+  print(&tokens2);
 
   empty_list(&tokens);
-
+  empty_list(&tokens2);
   return 0;
 }
 
