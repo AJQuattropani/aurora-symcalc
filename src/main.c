cdab4213c5a5ed79df340ca844d49e05b02e8b0d
AJ Quattropani <AJQuattropani@github.com>
2025-03-29 05:22:55 -0600

    Added a quick little interface for accepting command line input to do function interpreting and evaluation.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
+#include "interpret.h"
 
 int main(int argc, char *argv[]) {
-
-  return 0;
+  if (argc > 2) {
+    printf("Running %10s\n", argv[0]);
+    char fun[256];
+    double x;
+    if (sscanf(argv[1], "%255s", &fun[0]) == 0) return -5;
+    if (sscanf(argv[2], "%lf", &x) == 0) return -5;
+    struct Operation *oper = build_function(&fun[0], strlen(fun));
+    printf("%lf\n", a_evaluate(oper, x));
+    a_free_recursive(oper);
+    return 0;
+  }
+  return 4;
 }
