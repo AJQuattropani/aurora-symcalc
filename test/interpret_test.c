91dd166293d64277196eedc36dee37c182cc3ade
AJ Quattropani <AJQuattropani@github.com>
2025-03-29 05:01:47 -0600

    Added tests for base cases in interpreter.

--- test/interpret_test.c
+++ test/interpret_test.c
@@ -13,12 +13,12 @@ void test_for(const char* string)
 
 void test_build()
 {
+  {
   const char* string = "2_x+\\sin(x-2.4)";
   printf("%s\n", string);
   
   struct token_list tokens = parse_string_tokens(string, strlen(string));
   print(&tokens);
-  
   struct Operation *oper = least_significant_operation(tokens.head, NULL);
 
   double x = 2.3;
@@ -28,24 +28,59 @@ void test_build()
   EQUALS(y, y_exp);
   a_free_recursive(oper);
   empty_list(&tokens);
-
+  }
+  {
   const char* string2 = "x_2 + e_(2\\sin(x-1.5))-\\arctan(x/2)^(2-x)x";
-
   printf("%s\n", string2);
   
-  tokens = parse_string_tokens(string2, strlen(string2));
+  struct token_list tokens = parse_string_tokens(string2, strlen(string2));
   print(&tokens);
   
-  oper = least_significant_operation(tokens.head, NULL);
+  struct Operation *oper = least_significant_operation(tokens.head, NULL);
 
-  x = 2;
-  y = a_evaluate(oper, x);
+  double x = 2;
+  double y = a_evaluate(oper, x);
   
-  y_exp = b_add(b_log(x,2), b_sub(b_log(2.718281828459045,2*u_sin(b_sub(x,1.5))), b_mult(b_pow(u_arctan(b_div(x,2)),b_sub(2,x)), x)));
+  double y_exp = b_add(b_log(x,2), 
+                       b_sub(b_log(2.718281828459045,2*u_sin(b_sub(x,1.5))), b_mult(b_pow(u_arctan(b_div(x,2)),b_sub(2,x)), x)));
   EQUALS(y, y_exp);
   a_free_recursive(oper);
   empty_list(&tokens);
+  }
+  {
+  const char* string3 = "x";
+  printf("%s\n", string3);
+  struct token_list tokens = parse_string_tokens(string3, strlen(string3));
   
+  print(&tokens);
+  
+  struct Operation *oper = least_significant_operation(tokens.head, NULL);
+
+  double x = -596922;
+  double y = a_evaluate(oper, x);
+  
+  double y_exp = x;
+  EQUALS(y, y_exp);
+  a_free_recursive(oper);
+  empty_list(&tokens);
+  }
+  {
+  const char* string3 = "2";
+  printf("%s\n", string3);
+  struct token_list tokens = parse_string_tokens(string3, strlen(string3));
+  
+  print(&tokens);
+  
+  struct Operation *oper = least_significant_operation(tokens.head, NULL);
+
+  double x = -596922.455;
+  double y = a_evaluate(oper, x);
+  
+  double y_exp = 2;
+  EQUALS(y, y_exp);
+  a_free_recursive(oper);
+  empty_list(&tokens);
+  }
 
 }
 
