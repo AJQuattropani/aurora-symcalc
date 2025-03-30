cb082561cbfd03b97b496b46454c12ecd528f511
AJ Quattropani <AJQuattropani@github.com>
2025-03-30 02:20:11 -0600

    Implemented a system for parsing user input as command arguments and as console input.

---
+++ src/command_reader.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void read_file(const char *path);
+
+void read_from_in();
+
+
+
+
+
+
--- src/main.c
+++ src/main.c
@@ -1,16 +1,18 @@
+#include "command_reader.h"
 #include "interpret.h"
 
-int main(int argc, char *argv[]) {
-  if (argc > 2) {
-    printf("Running %10s\n", argv[0]);
-    char fun[256];
-    double x;
-    if (sscanf(argv[1], "%255s", &fun[0]) == 0) return -5;
-    if (sscanf(argv[2], "%lf", &x) == 0) return -5;
-    struct Operation *oper = build_function(&fun[0], strlen(fun));
-    printf("%lf\n", a_evaluate(oper, x));
-    a_free_recursive(oper);
-    return 0;
-  }
-  return 4;
+int main(const int argc, const char *argv[]) {
+  if (argc > 1)
+    for (int argi = 1; argi < argc; argi++) {
+      char location[1024];
+      if (sscanf(argv[argi], "%1023s", location) == 0) {
+        fprintf(stderr, "Invalid argument.");
+        return -1;
+      }
+      read_file(location);
+    }
+
+  read_from_in();
+
+  return 0;
 }
