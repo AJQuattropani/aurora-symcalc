2c4b3862edcf8340b6faa67f8798ddbec8c3bbfc
AJ Quattropani <AJQuattropani@github.com>
2025-04-16 16:34:41 -0600

    Suppressing unused warnings in nonsense free statements.

--- src/objects/initializers.c
+++ src/objects/initializers.c
@@ -4,18 +4,18 @@ Object as_mfcontext(context_modifier mod) {
   return (Object){.mContext = mod, .ty = CONTEXT};
 }
 
-void free_mfcontext(mf_context *mod) { mod = NULL; }
+void free_mfcontext([[maybe_unused]] mf_context *mod) { mod = NULL; }
 
 Object as_bopliteral(b_opliteral opr) {
   return (Object){.bOperation = opr, .ty = BOPER};
 }
 
-void free_bopliteral(b_opliteral *opr) { opr = NULL; }
+void free_bopliteral([[maybe_unused]] b_opliteral *opr) { opr = NULL; }
 
 Object as_uopliteral(u_opliteral opr) {
   return (Object){.uOperation = opr, .ty = UOPER};
 }
 
-void free_uopliteral(u_opliteral *opr) { opr = NULL; }
+void free_uopliteral([[maybe_unused]] u_opliteral *opr) { opr = NULL; }
 
 
