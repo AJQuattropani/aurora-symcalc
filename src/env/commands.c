2154a1b8ba9f21eb056dfdcc432ccc86111c18b9
AJ Quattropani <AJQuattropani@github.com>
2025-04-18 17:34:39 -0600

    Removed cleanup responsibility from exit command.

--- src/env/commands.c
+++ src/env/commands.c
@@ -1,20 +1,19 @@
 #include "commands.h"
 
 void exit_env(env *context, [[maybe_unused]] vList *args) {
-  empty_map(&context->map);
-  
-  context->current_file = NULL;
   context->status = EXIT;
+  printf("EXIT!\n");
 }
+
 void reset_env(env *context, [[maybe_unused]] vList *args) {
   empty_map(&context->map);
   default_map(&context->map);
-
   context->status = OK;
+  printf("RESET!\n");
 }
 
 void open_file([[maybe_unused]] env *context, [[maybe_unused]] vList *args) {
-  
+  printf("OPEN!\n");
 
 
 }
