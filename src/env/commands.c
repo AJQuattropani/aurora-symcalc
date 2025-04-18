720241b00657c771e855d542918f70a5b4412fec
AJ Quattropani <AJQuattropani@github.com>
2025-04-18 01:11:52 -0600

    Suppress warnings for unused parameters.

--- src/env/commands.c
+++ src/env/commands.c
@@ -1,19 +1,19 @@
 #include "commands.h"
 
-void exit_env(env *context, vList *args) {
+void exit_env(env *context, [[maybe_unused]] vList *args) {
   empty_map(&context->map);
   
   context->current_file = NULL;
   context->status = EXIT;
 }
-void reset_env(env *context, vList *args) {
+void reset_env(env *context, [[maybe_unused]] vList *args) {
   empty_map(&context->map);
   default_map(&context->map);
 
   context->status = OK;
 }
 
-void open_file(env *context, vList *args) {
+void open_file([[maybe_unused]] env *context, [[maybe_unused]] vList *args) {
   
 
 
