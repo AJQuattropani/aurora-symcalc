0930f641a2a033b00856a6756bb38162ded317b3
AJ Quattropani <AJQuattropani@github.com>
2025-04-19 02:16:14 -0600

    Added documentation for current commands and their use. Implemented reader function for vector input, and function for defining variables in the environment.

--- src/env/commands.c
+++ src/env/commands.c
@@ -10,8 +10,55 @@ void reset_env(env *context, [[maybe_unused]] const token_array *args) {
   context->status = OK;
 }
 
-//void open_file(env *context, [[maybe_unused]] vList *args) {
-  
+void print_env(env *context, [[maybe_unused]] const token_array *args) {
+  print_map(&context->map);
+}
 
-//}
+void define_object([[maybe_unused]] env *context, const token_array *args) {
+  if (4 > args->size) {
+    fprintf(stderr, "[SKIPPED] Insufficient arguments for set call.\n");
+    goto catch_error;
+  }
+  Object *into = &args->data[1].token->value;
+  if (NONE != into->ty) {
+    _key *str = &args->data[1].token->key;
+    fprintf(stderr, "[SKIPPED] Writing to existing type \"%.*s\" not supported.\n", (int)str->size, str->cstring);
+    goto catch_error;
+  }
 
+  Object *read_type = &args->data[2].token->value;
+  if (READER != read_type->ty) {
+    fprintf(stderr, "[SKIPPED] Invalid type.\n");
+    goto catch_error;
+  }
+  r_macro read_macro = read_type->reader;
+  *into = read_macro(&(const token_array){
+      .data = args->data + 3,
+      .capacity = 0,
+      .size = args->size -
+              3}); // read in remaining args as either function or vector
+
+catch_error:
+  for (size_t i = 0; i < args->size; i++) {
+    token *curr = &args->data[i];
+    if (NONE == curr->token->value.ty)
+      remove_node(curr->token);
+  } // clean up newly-hashed nodes
+}
+
+Object read_vector(const token_array *args) {
+  vd_literal vector = alloc_vdliteral(args->size);
+  for (size_t i = 0; i < args->size; i++) {
+    token *curr = &args->data[i];
+    if (0 >= sscanf(curr->token->key.cstring, "%lf", &vector.data[i])) {
+      fprintf(stderr,
+              "[SKIPPED] Non-double definition provided for input %ld.\n", i);
+      vector.data[i] = 0;
+      continue;
+    }
+  }
+  return as_vdliteral(&vector);
+}
+
+// void open_file(env *context, [[maybe_unused]] vList *args) {
+// }
--- src/env/commands.h
+++ src/env/commands.h
@@ -8,10 +8,33 @@
  * Applicable functions for CONTEXT type
  */
 
+/*
+* FLAGS ENVIRONMENT TO EXIT.
+*/
 void exit_env(env *context, const token_array *args);
 
+/*
+* FLAGS ENVIRONMENT TO RESET.
+*/
 void reset_env(env *context, const token_array *args);
+
+/*
+ * PRINTS ALL ENVIRONMENT VARIABLES.
+ */
+void print_env(env *context, [[maybe_unused]] const token_array *args);
 
 //void open_file(env *context, vList *args);
 
+/**
+* Makes a new object.
+* arg 0 : set
+* arg 1 : name
+* arg 2 : method to read (vector, function, etc.)
+* arg 3 : values to read in
+*/
+void define_object(env *context, const token_array *args);
 
+/**
+* Instructs to read remaining tokens as a vector.
+*/
+Object read_vector(const token_array *args);
