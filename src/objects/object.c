0913eef59e51dcdd03477536d705ab760936c1bd
AJ Quattropani <AJQuattropani@github.com>
2025-04-19 17:13:21 -0600

    sprint_object method for polymorphic writing of data  to gstring buffer.

--- src/objects/object.c
+++ src/objects/object.c
@@ -1,5 +1,32 @@
 #include "object.h"
 
+void sprint_object(gString *gstr, const Object *o) {
+  switch (o->ty) {
+  case VECTOR:
+    sprint_vector(gstr, o->vLiteral);
+    break;
+  case CONTEXT:
+    g_append_back(gstr, "CONTEXT", 9);
+    break;
+  case BOPER:
+    g_append_back(gstr, "BINARYOPR", 9);
+    break;
+  case UOPER:
+    g_append_back(gstr, "UNARYOPR", 8);
+    break;
+  case FUNC:
+    g_append_back(gstr, "todofunc", 8);
+    break;
+    //sprint_function(gstr, o->fObject);
+  case READER: 
+    g_append_back(gstr, "READER", 6);
+    break;
+  case NONE:
+    g_append_back(gstr, "NONE", 4);
+    break;
+  }
+}
+
 void free_object(Object *o) {
   switch (o->ty) {
   case VECTOR:
--- src/objects/object.h
+++ src/objects/object.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <string.h>
 
+#include "../strings/gstring.h"
+
 #include "initializers.h"
 #include "vector.h"
 #include "function.h"
+
+void sprint_object(gString *gstr, const Object *o);
 
 void free_object(Object *o);
