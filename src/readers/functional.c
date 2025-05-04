4819a4d23fea0148ac547aec69c54ca5ea507a66
AJ Quattropani <AJQuattropani@github.com>
2025-05-04 07:40:19 -0600

    Removed deprecated code.

--- src/readers/functional.c
+++ src/readers/functional.c
@@ -67,31 +67,12 @@ Object function_eval(f_object *fun, const token_array *args) {
         goto cleanup;
       }
     }
-    // hybrid buffer allocates all memory for vectors and literals
-    //vd_literal *out_cache =
-    //    (vd_literal *)malloc(sizeof(vd_literal) * fun->attr.depth);
-    //if (NULL == out_cache) {
-    //  fprintf(stderr, "[ERROR] malloc failed in %s\n", __func__);
-    //  exit(1);
-    //}
     
     vector_list out_cache = alloc_vdlist(fun->attr.depth, inp_size);
-
-    //for (size_t i = 0; i < fun->attr.depth; i++) {
-    //  out_cache[i] = alloc_vdliteral(inp_size);
-    //}
-    //evaluate_function_imp(fun->root, out_cache, inp_args);
     evaluate_function_imp(fun->root, out_cache.data, inp_args);
-
-    //output = (Object){.ty = VECTOR, .vLiteral = copy_vdliteral(&out_cache[0])};
     output = (Object){.ty = VECTOR, .vLiteral = copy_vdliteral(&out_cache.data[0])};
-
-    //for (size_t i = 0; i < fun->attr.depth; i++) {
-    //  free_vdliteral(&out_cache[i]);
-    //}
-    //free(out_cache);
-
     free_vdlist(&out_cache);
+
   cleanup:
     free(inp_args);
   }
