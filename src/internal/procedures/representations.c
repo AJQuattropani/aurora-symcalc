798596faa34660af25c26f03716d181c81ec5d2d
AJ Quattropani <AJQuattropani@github.com>
2025-05-07 17:54:03 -0600

    Added a representation of the function as a tree. Uses tabs to denote the hierarchy of parents and children in the AST.

--- src/internal/procedures/representations.c
+++ src/internal/procedures/representations.c
@@ -1,8 +1,27 @@
 #include "representations.h"
 
-void tree_command(Object *restrict obj, token_array *restrict args) {
-
-
-
+static void print_node_recurse(gString *restrict gstr, f_node *restrict curr, int ntabs) {
+  g_put_char(gstr, '\n', 1);
+  g_put_char(gstr, ' ', ntabs);
+  switch (curr->ty) {
+  case BINARY:
+    g_append_back(gstr, curr->name.cstring, curr->name.size);
+    print_node_recurse(gstr, curr->bf.left, ntabs + 2);
+    print_node_recurse(gstr, curr->bf.right, ntabs + 2);
+    break;
+  case UNARY:
+    g_append_back(gstr, curr->name.cstring, curr->name.size);
+    print_node_recurse(gstr, curr->uf.in, ntabs + 2);
+    break;
+  case CONSTANT: 
+    g_append_back(gstr, curr->name.cstring, curr->name.size);
+    break;
+  case IDENTITY:
+    g_append_back(gstr, curr->name.cstring, curr->name.size);
+    break;
+  }
 }
 
+void sprint_tree(gString * restrict gstr, f_object * restrict func) {
+  print_node_recurse(gstr, func->root, 0); 
+}
