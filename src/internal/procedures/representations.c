b596d49282fa20cf491ebb990f2b66729a28dc85
AJ Quattropani <AJQuattropani@github.com>
2025-05-07 17:05:22 -0600

    Added files for upcoming representations, a way of representing functions in more ways.

---
+++ src/internal/procedures/representations.c
@@ -0,0 +1,8 @@
+#include "representations.h"
+
+void tree_command(Object *restrict obj, token_array *restrict args) {
+
+
+
+}
+
---
+++ src/internal/procedures/representations.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "../../objects/function.h"
+
+
+void tree_command(Object *obj, token_array *args);
+
