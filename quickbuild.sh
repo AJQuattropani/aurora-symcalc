2584024fe5307dc0084ff17ff03cd6423570e402
AJ Quattropani <AJQuattropani@github.com>
2025-05-09 20:26:13 -0600

    Pulled build files creation out of if statements.

--- quickbuild.sh
+++ quickbuild.sh
@@ -5,15 +5,15 @@ echo "Building ..."
 if [ ! -d "${DEBUG_DIR}" ] ; then
 echo "Generating Directory at ${DEBUG_DIR}"
 mkdir $DEBUG_DIR
-cmake -DCMAKE_BUILD_TYPE=Debug -S . -B $DEBUG_DIR
 fi
+cmake -DCMAKE_BUILD_TYPE=Debug -S . -B $DEBUG_DIR
 cmake --build $DEBUG_DIR
 echo "Built Debug at ${DEBUG_DIR}"
 
 if [ ! -d "${RELEASE_DIR}" ] ; then
 echo "Generating Directory at ${RELEASE_DIR}"
 mkdir $RELEASE_DIR
-cmake -DCMAKE_BUILD_TYPE=Release -S . -B $RELEASE_DIR
 fi
+cmake -DCMAKE_BUILD_TYPE=Release -S . -B $RELEASE_DIR
 cmake --build $RELEASE_DIR
 echo "Built Release at ${RELEASE_DIR}"
